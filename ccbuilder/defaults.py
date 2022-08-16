f8a57ed73ce4dd6a6bcf9860389907335715121a
Theodoros Theodoridis <theodort@inf.ethz.ch>
2022-08-16 13:34:28 +0200

    Expose default paths

---
+++ ccbuilder/defaults.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DEFAULT_PREFIX_PARENT_PATH = Path.home() / ".cache" / "ccbuilder-compilers"
+DEFAULT_PATCH_DIR = Path.home() / ".cache" / "ccbuilder-patches"
+DEFAULT_REPOS_DIR = Path.home() / ".cache" / "ccbuilder-repos"
